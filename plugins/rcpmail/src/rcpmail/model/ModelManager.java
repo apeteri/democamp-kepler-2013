72bb0691a7043f6b60f9dbc395ee4a9cba531f37
András Péteri <apeteri@b2international.com>
2013-06-11 00:56:15 +0200

    Add transactional operations to ModelManager

--- plugins/rcpmail/src/rcpmail/model/ModelManager.java
+++ plugins/rcpmail/src/rcpmail/model/ModelManager.java
@@ -15,6 +15,7 @@
 package rcpmail.model;
 
 import java.util.Date;
+import java.util.List;
 
 import org.eclipse.emf.cdo.CDOObject;
 import org.eclipse.emf.cdo.eresource.CDOResource;
@@ -32,8 +33,6 @@ import org.eclipse.net4j.connector.IConnector;
 import org.eclipse.net4j.util.WrappedException;
 import org.eclipse.net4j.util.container.IPluginContainer;
 import org.eclipse.net4j.util.lifecycle.Lifecycle;
-
-import rcpmail.model.util.FolderAdapter;
 
 /**
  * @author Eike Stepper
@@ -69,6 +68,46 @@ public class ModelManager extends Lifecycle {
 		});
 	}
 
+	public void trashMessage(Message msg) {
+		modify(msg, new ITransactionalOperation<Message>() {
+			@Override
+			public Object execute(Message object) {
+				Folder trash = getTrash(object.getFolder().getServer().getFolders());
+				Folder current = object.getFolder();
+				if (trash != current) {
+					trash.getMessages().add(object);
+				} else {
+					trash.getMessages().remove(object);
+				}
+				return object;
+			}
+
+			private Folder getTrash(List<Folder> folders) {
+				for (Folder folder : folders) {
+					if ("Trash".equals(folder.getName())) {
+						return folder;
+					}
+				}
+				return null;
+			}
+		});
+	}
+	
+	public void markAndMoveMessage(Message msg) {
+		modify(msg, new ITransactionalOperation<Message>() {
+			@Override
+			public Message execute(Message object) {
+				object.setSpam(true);
+				Folder junk = object.getFolder().getServer().getJunkFolder();
+				Folder current = object.getFolder();
+				if (current != junk) {
+					junk.getMessages().add(object);
+				}
+				return object;
+			}
+		});
+	}
+
 	public synchronized CDOResource getServerResource() {
 
 		if (null == serverResource) {
@@ -78,55 +117,7 @@ public class ModelManager extends Lifecycle {
 
 				try {
 					CDOResource resource = transaction.createResource(path);
-
-					Server initialServer = createServer();
-					initialServer.setHostname("mailserver.hu");
-					initialServer.setUsername("user1");
-					initialServer.setPort(25);
-
-					Message message = ModelFactory.eINSTANCE.createMessage();
-
-					message.setFrom("\"eBay Cash Machine\" <connectso@atechonline.net>");
-					message.setDate(new Date().toString());
-					message.setSpam(true);
-					message.setSubject("[Spam] Quit Your Day Job Within 30 Days");
-					message.setBody("Ever heard of the eBay ca$h machine ?\r\n"
-							+ "\r\n"
-							+ "\"How would you like an extra $250 - $1000 a week on eBay with 15 minutes of\r\n"
-							+ "your time ?\"\r\n"
-							+ "\r\n"
-							+ " I've made it my job to help people succeed online.\r\n"
-							+ "I'm constantly on the lookout for the best ways and means to make your job\r\n"
-							+ "simpler, and I pass the good stuff on to you.\r\n"
-							+ "\r\n"
-							+ "I have developed the eBay Cash Machine - it allows everyone to make a great\r\n"
-							+ "income on eBay 99% automatically. It only takes a few minutes to set up and\r\n"
-							+ "once that is done you will have your own eBay Businesses that literally run\r\n"
-							+ "on auto-pilot!\r\n"
-							+ "You just wait for the money to come in!\r\n"
-							+ "\r\n"
-							+ "I have found a foolproof method how anyone can easily make an extra $250,\r\n"
-							+ "$500 or even $1000 per week on eBay using my eBay Ca$h Machine.\r\n"
-							+ "\r\n"
-							+ "Now before I get to deep into the details let me tell you what the eBay\r\n"
-							+ "Ca$h Machine ISN'T.\r\n"
-							+ "\r\n"
-							+ "It is NOT a promote my affiliate program to make m.oney!\r\n"
-							+ "It is NOT a NOT A Get-Rich Q.uick Scheme!\r\n"
-							+ "It is NOT a Face To Face Selling program of Any Kind!\r\n"
-							+ "It is NOT a Trial Offer to sign up for!\r\n"
-							+ "It is NOT some garbage product that refers you to purchase other garbage\r\n"
-							+ "products.\r\n"
-							+ "It is NOT a M.LM, a Pyramid Scheme, Gifting or anything else like that.\r\n"
-							+ "It is NOT like anything you have ever seen.\r\n"
-							+ "\r\n"
-							+ "For Full Details please read the attached .html file\r\n"
-							+ "\r\n"
-							+ "To Unsubscribe please read the attached Unsubscribe.txt");
-
-					initialServer.getDefaultSelection().getMessages()
-							.add(message);
-
+					Server initialServer = createInitialServer();
 					resource.getContents().add(initialServer);
 					transaction.commit();
 
@@ -143,10 +134,66 @@ public class ModelManager extends Lifecycle {
 		return serverResource;
 	}
 
+	private Server createInitialServer() {
+		
+		Server initialServer = createServer();
+		initialServer.setHostname("mailserver.hu");
+		initialServer.setUsername("user1");
+		initialServer.setPort(25);
+
+		Message message = ModelFactory.eINSTANCE.createMessage();
+
+		message.setFrom("\"eBay Cash Machine\" <connectso@atechonline.net>");
+		message.setDate(new Date().toString());
+		message.setSpam(true);
+		message.setSubject("[Spam] Quit Your Day Job Within 30 Days");
+		message.setBody("Ever heard of the eBay ca$h machine ?\r\n"
+				+ "\r\n"
+				+ "\"How would you like an extra $250 - $1000 a week on eBay with 15 minutes of\r\n"
+				+ "your time ?\"\r\n"
+				+ "\r\n"
+				+ " I've made it my job to help people succeed online.\r\n"
+				+ "I'm constantly on the lookout for the best ways and means to make your job\r\n"
+				+ "simpler, and I pass the good stuff on to you.\r\n"
+				+ "\r\n"
+				+ "I have developed the eBay Cash Machine - it allows everyone to make a great\r\n"
+				+ "income on eBay 99% automatically. It only takes a few minutes to set up and\r\n"
+				+ "once that is done you will have your own eBay Businesses that literally run\r\n"
+				+ "on auto-pilot!\r\n"
+				+ "You just wait for the money to come in!\r\n"
+				+ "\r\n"
+				+ "I have found a foolproof method how anyone can easily make an extra $250,\r\n"
+				+ "$500 or even $1000 per week on eBay using my eBay Ca$h Machine.\r\n"
+				+ "\r\n"
+				+ "Now before I get to deep into the details let me tell you what the eBay\r\n"
+				+ "Ca$h Machine ISN'T.\r\n"
+				+ "\r\n"
+				+ "It is NOT a promote my affiliate program to make m.oney!\r\n"
+				+ "It is NOT a NOT A Get-Rich Q.uick Scheme!\r\n"
+				+ "It is NOT a Face To Face Selling program of Any Kind!\r\n"
+				+ "It is NOT a Trial Offer to sign up for!\r\n"
+				+ "It is NOT some garbage product that refers you to purchase other garbage\r\n"
+				+ "products.\r\n"
+				+ "It is NOT a M.LM, a Pyramid Scheme, Gifting or anything else like that.\r\n"
+				+ "It is NOT like anything you have ever seen.\r\n"
+				+ "\r\n"
+				+ "For Full Details please read the attached .html file\r\n"
+				+ "\r\n"
+				+ "To Unsubscribe please read the attached Unsubscribe.txt");
+
+		initialServer
+				.getDefaultSelection()
+				.getMessages()
+				.add(message);
+		
+		return initialServer;
+	}
+
 	public Object getDefaultSelection() {
 		EList<EObject> servers = getServerResource().getContents();
-		return servers.size() > 0 ? ((Server) servers.get(0))
-				.getDefaultSelection() : null;
+		return servers.size() > 0 
+				? ((Server) servers.get(0)).getDefaultSelection() 
+				: null;
 	}
 
 	public Server createServer() {
@@ -167,7 +214,6 @@ public class ModelManager extends Lifecycle {
 
 	private Folder createFolder(String name) {
 		Folder folder = ModelFactory.eINSTANCE.createFolder();
-		folder.eAdapters().add(new FolderAdapter());
 		folder.setName(name);
 		return folder;
 	}
